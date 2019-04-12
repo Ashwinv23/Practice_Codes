8d6406ed43be8869f5f79d4bb9fc09de03cd1535
Ashwin Vijendra <ack4and@gmail.com>
2019-04-12 20:40:25 +0530

    adding light to object

--- htmlcss/stomachview.js
+++ htmlcss/stomachview.js
@@ -1,3 +1,4 @@
+'use strict';
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x000000 );
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 2, 1000);
@@ -5,7 +6,7 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeigh
 camera.position.set( 0, 10, 0 );
 camera.position.z = 50;
 var texture = new THREE.TextureLoader().load( "./images/stomach.png" );
-var material = new THREE.MeshBasicMaterial( { map: texture } );
+var material = new THREE.MeshPhongMaterial( { map: texture, shininess: 10 } );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,10 +15,23 @@ var stomachObj;
 var stomachAngle = Math.PI / 2;
 var target = Math.PI / 2;
 var isRotating = false;
+// var directionalLight = new THREE.DirectionalLight( 0xFFFFFF, 1.5 );
+// directionalLight.position.set( 10, 20, 50 );
+// // directionalLight.target(0, 30, 0);
+// scene.add( directionalLight );
+// var directionalLight1 = new THREE.DirectionalLight( 0xFFFFFF, -1.5 );
+// directionalLight1.position.set( 0, 20, -50 );
+// // directionalLight1.target(0, 30, 0);
+// scene.add( directionalLight1 );
+
+const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(3, 8, 4);
+  scene.add(light);
 
 loader.load('model.obj', function (obj) {
   stomachObj = obj;
-  // location.reload(true);
   event.preventDefault();
   for(var i = 0; i < obj.children.length; i++) {
     obj.children[i].material = material;
@@ -28,24 +42,23 @@ loader.load('model.obj', function (obj) {
   scene.add(obj);
 });
 
-  function moveObj(zaxis) {
-    // stomachObj.rotation.set(-Math.PI/2, -0.02, zaxis);
-    isRotating = true;
-    target = zaxis;
-  }
+function moveObj(zaxis) {
+  // stomachObj.rotation.set(-Math.PI/2, -0.02, zaxis);
+  isRotating = true;
+  target = zaxis;
+}
 
 function render() {
   requestAnimationFrame(render);
-  if(isRotating) {
-    var oldR = stomachObj.rotation;
-
-    stomachAngle += 0.01;
-    stomachObj.rotation.set(oldR.x, oldR.y, stomachAngle);
-    if(stomachAngle > target) {
-      isRotating = false;
+  if (isRotating) {
+    if (target < stomachAngle) {
+      stomachAngle -= 0.075;
     }
+    if (target > stomachAngle) {
+      stomachAngle += 0.075;
+    }
+    stomachObj.rotation.z = stomachAngle;
   }
-
   renderer.render(scene, camera);
 }
 
